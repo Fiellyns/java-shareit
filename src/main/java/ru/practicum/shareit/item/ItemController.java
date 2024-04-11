19e8faf5924b837dd80b3f6f6618d8120aaf3780
Fiellyns <fiellynss@gmail.com>
2024-04-11 16:34:37 +0500

    fix: fix tests

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -9,7 +9,6 @@ import ru.practicum.shareit.forDto.Create;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 
-import javax.validation.Valid;
 import java.util.Collection;
 
 
